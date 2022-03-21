9b77f3a8a971f8372baa5c52463421c6a96a965b
yliess86 <hatiyliess86@gmail.com>
2022-03-21 23:25:55 +0100

    [Feature] Examples for Configuration

---
+++ examples/config.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from torchbooster.config import (
+    BaseConfig,
+    EnvironementConfig,
+    LoaderConfig,
+    OptimizerConfig,
+    SchedulerConfig,
+)
+
+
+@dataclass
+class DatasetConfig(BaseConfig):
+    name: str
+    path: Path
+    resolution: int
+
+
+@dataclass
+class Config(BaseConfig):
+    env: EnvironementConfig
+    # dataset: DatasetConfig   #TODO Fix Type Evaluation
+    loader: LoaderConfig
+    optim: OptimizerConfig
+    scheduler: SchedulerConfig
+
+
+if __name__ == "__main__":
+    from pathlib import Path
+
+
+    path = Path("examples/config/base.yml")
+    conf = Config.load(path)
+    print(conf)
