dc32649f2b029075f0ce4cff041ca6e8555db401
yliess86 <hatiyliess86@gmail.com>
2022-03-22 13:18:07 +0100

    [Feature] Added Setup File

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from torchbooster import __version__
+
+
+setup(
+    name="torchbooster",
+    version=__version__,
+    description="A simple and yet practical pytorch booster library to help with bootstraping and reproducing research.",
+    author="Yliess Hati and Gregor Jouet",
+    author_email="{yliess.hati, gregor.jouet}@devinci.fr",
+    packages=["torchbooster"],
+    package_dir={"torchbooster": "torchbooster"},
+    keywords=["research", "deep-learning", "reproducible-research", "python3", "pytorch"],
+    license="MIT",
+)
